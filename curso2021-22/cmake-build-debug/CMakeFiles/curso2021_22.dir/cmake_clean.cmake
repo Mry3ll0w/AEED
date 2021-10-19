c8bfc89505465a13ed71036047fb901f45a22107
Mry3ll0w <48240649+Mry3ll0w@users.noreply.github.com>
2021-10-19 12:52:35 +0200

    P1 lista
    Falta por ser roasteada por DLH

---
+++ curso2021-22/cmake-build-debug/CMakeFiles/curso2021_22.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curso2021_22.dir/main.cpp.obj"
+  "curso2021_22.exe"
+  "curso2021_22.exe.manifest"
+  "curso2021_22.pdb"
+  "libcurso2021_22.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curso2021_22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- curso2021-22/p1.h
+++ curso2021-22/p1.h
@@ -2,10 +2,13 @@
 #ifndef CURSO2021_22_P1_H
 #define CURSO2021_22_P1_H
 #include <iostream>
+#include <cmath>
 #include <ctime>
+#include <algorithm>
+#include <vector>
 
 int ej1(int l_superior, int l_inferior){
-    //inicializamos la seed de rand
+
     return rand() % (l_superior - l_inferior + 1)+l_inferior;
 }
 
@@ -24,4 +27,45 @@ double ej2(long int N = 10000000){
     return (double)(ni/tamano_muestra); //devuelve la frecuencia relativa
 }
 
+/**
+ *
+ * @param a ==> limite superior del intervalo para generar el numero pseudo aleatorio
+ * @param b ==> limite inferior del intervalo para generar el numero pseudo Aleatorio
+ * @return numero generado por estos limites (double)
+ */
+double ej3(const double&a , const double& b){
+    return b + ((double)rand() / RAND_MAX) * (a - b);
+}
+/**
+ *
+ * @return la media y = 4 ·√(1 − x^2)
+ */
+double ej4(const double N =10000000 ){
+    double media;
+    media=0;
+    size_t it;
+    it = 0;
+    while(it < N){
+        media+=4 * sqrt(1-pow(ej3(1,0),2));//4 ·√(1 − x^2)
+        it++;
+    }
+    return media/N; //devuelve la media entre el numero de iteraciones
+}
+
+//ejercicio 5
+size_t ej5(){
+    std::vector<size_t>v={6,2,3,4,1,5};
+    std::vector<size_t>v2= {1,2,3,4,5,6};
+    size_t contador;
+    contador = 0;
+    size_t N = 10000000 ;
+    //incio del algoritmo
+    while(N>0){
+        std::random_shuffle(v.begin(),v.end());
+        if(v==v2)
+            contador++;
+        N--;
+    }
+    return contador;
+}
 #endif //CURSO2021_22_P1_H
