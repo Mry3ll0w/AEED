7bd4d293e387c3bf0cdc81736bc8b0f7b44d9017
Mry3ll0w <48240649+Mry3ll0w@users.noreply.github.com>
2021-10-22 14:04:43 +0200

    Eliminados vector de alto nivel por arrays en ej5

--- curso2021-22/main.cpp
+++ curso2021-22/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "p1.h"
 int main() {
-    srand(time(NULL));//SOLO DEBE SER LLAMADA UNA Única vez por mainº
+    srand(time(0));//SOLO DEBE SER LLAMADA UNA Única vez por mainº
     std::cout<< ej5()<<std::endl;
     return 0;
 }
--- curso2021-22/p1.h
+++ curso2021-22/p1.h
@@ -6,14 +6,24 @@
 #include <ctime>
 #include <algorithm>
 #include <vector>
+#include <cstring>
+/*
+Notas de Clase Practica 1( 22/ 10 / 2021)
+    
+    - No cae lo que se da en clase , sale lo que se "deduce" de las mismas.
+    - Argumentar lo implementado, o al menos razonarlo, de cara a las revisiones.
 
-int ej1(int l_superior, int l_inferior){
 
-    return rand() % (l_superior - l_inferior + 1)+l_inferior;
+*/
+
+/* ----------------------------- IMPLEMENTACION DE LOS EJERCICIOS ----------------------------- */
+
+inline int ej1(const int l_superior, const int l_inferior){
+    return rand() % (l_superior - l_inferior + 1) + l_inferior;
 }
 
-//Tirada de dados 10 000 000, utilizando la funcion anterior (ej19
-double ej2(long int N = 10000000){
+//Tirada de dados 10 000 000, utilizando la funcion anterior (ej1)
+const double ej2(long int N = 10000000){
     double ni;//Control de los numeros obtenidos en cada tirada
     ni=0;
     long int tamano_muestra;//Control del tamaño total de muestra
@@ -33,36 +43,44 @@ double ej2(long int N = 10000000){
  * @param b ==> limite inferior del intervalo para generar el numero pseudo Aleatorio
  * @return numero generado por estos limites (double)
  */
-double ej3(const double&a , const double& b){
+inline const double ej3(double a , double b){
+    b = std::abs(b);
+    a = std::abs(a);
+
     return b + ((double)rand() / RAND_MAX) * (a - b);
 }
 /**
  *
  * @return la media y = 4 ·√(1 − x^2)
  */
-double ej4(const double N =10000000 ){
+const double ej4(const double N =10000000 ){
+    //variables del ejercicio
     double media;
     media=0;
     size_t it;
     it = 0;
+
     while(it < N){
         media+=4 * sqrt(1-pow(ej3(1,0),2));//4 ·√(1 − x^2)
         it++;
     }
+
     return media/N; //devuelve la media entre el numero de iteraciones
 }
 
 //ejercicio 5
-size_t ej5(){
-    std::vector<size_t>v={6,2,3,4,1,5};
-    std::vector<size_t>v2= {1,2,3,4,5,6};
+const size_t ej5(){
+    
+    int v[6]={6,2,3,4,1,5};
+    int v2[6]= {1,2,3,4,5,6};
     size_t contador;
     contador = 0;
-    size_t N = 10000000 ;
+    size_t N;
+    N = 10000000; 
     //incio del algoritmo
     while(N>0){
-        std::random_shuffle(v.begin(),v.end());
-        if(v==v2)
+        std::random_shuffle(v,v+6);
+        if(std::memcmp(v,v2,6) == 0)
             contador++;
         N--;
     }
