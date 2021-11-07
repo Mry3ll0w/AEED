138b33aef4d1cb4d17ef8023a20a8c0c17bda4e3
Antonio Roldan Andrade <48240649+mry3ll0w@users.noreply.github.com>
2021-11-07 12:24:36 +0100

    Cronometro a full ( V2, algunas mejoras)

--- curso2021-22/main.cpp
+++ curso2021-22/main.cpp
@@ -3,12 +3,15 @@
 
 int main() {
     srand(time(0));//SOLO DEBE SER LLAMADA UNA Única vez por mainº
-    int *v = nullptr;
-    //std::cout << tiempo_intercambio() << std::endl;
+
+    std::cout << tiempo_intercambio() << std::endl;
     //std::cout << tiempo_seleccion() <<std::endl;
+
+    /*
     for(auto i: ordenaciones_total()){
         std::cout << i << std::endl;
     }
+    */
 
-    return 0;
+        return 0;
 }
