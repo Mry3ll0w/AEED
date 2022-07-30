dc8a19cf0f93a93c1419c15071476215fc214d06
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-07-30 16:33:36 +0200

    Ejercicio Junio de 2022

---
+++ Sept2022/jun2022.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <string>
+#include "tads/Cola_Din.h"
+#include "tads/ListaEnla.h"
+#include <cassert>
+
+using namespace std;
+
+typedef std::string codigo;
+struct Usuario{
+    ColaDin<codigo> Normal,Prioridad;
+    string ID;
+    bool operator==(const Usuario& other) const { return (ID == other.ID)}
+};
+
+class Impresora{
+    ColaDin<Usuario> c;
+    int max_usuarios, ocupados;//Se debe hacer usando cola vect pero no tengo la implementacion
+public:
+    Impresora(int n):max_usuarios(n),libres(0) { }
+    void insertar_trabajo(const Usuario& u, const std::string & t,bool urgencia);
+    void eliminar_trabajo_a_imprimir();
+    void cancelar_trabajos_usuario(const Usuario& u);
+};
+
+
+int main(int argc, char **argv){
+    return 0;
+}
+
+void Impresora::insertar_trabajo(const Usuario& u,cons std::string & t, bool urgencia){
+    
+
+    //Comprobamos que el usuario no exista
+    ColaDin<Usuario> t = c;
+
+    while ( !t.vacia() && t.frente() != u)
+        t.pop();
+    
+    if (t.vacia()){//Si t esta vacia el usuario no existe
+
+        if(ocupados < max_usuarios){
+            ocupados ++;
+            if(urgencia){
+                u.Prioridad.push(t);
+            }
+            else{
+                u.Normal.push(t);
+            }
+            c.push(u);
+        }
+        else{
+            cout<<"No hay espacio para el usuario en la cola de impresion";
+        }
+
+    }
+    else {//El usuario ya esta en la cola de impresion
+
+        ColaDin<Usuario> parser;
+
+        while(!c.vacia() && c.frente() == u)
+            parser.push(c.frente());c.pop();
+        
+        u = c.frente();//Metemos el elemento usuario, para obtener las colas
+
+        if(urgencia){
+                u.Prioridad.push(t);
+        }
+        else{
+            u.Normal.push(t);
+        }
+        c.pop();//Sacamos el usuario buscado
+        parser.push(u);
+        //Metemos el resto de los elementos en la cola original
+        while (!c.vacia()){
+            parser.push(c.frente());
+            c.pop();
+        }
+
+        c = parser;//Metemos la pila lista con los elementos en orden y actualizado
+    }
+    
+
+}
+
+void Impresora::eliminar_trabajo_a_imprimir(){
+    
+    assert(!c.vacia()); //No puede estar vacia
+    auto u = c.frente();//Sacamos al primero de la cola
+    c.pop();
+    ocupados --;
+
+    //Imprimimos según prioridad
+    if(!u.Prioridad.vacia()){
+        u.Prioridad.pop();
+    }
+    else{
+        u.Normal.pop();
+    }
+
+    //Tras imprimir el trabajo que toca, si no esta vacias las listas volvemos el user al final
+    if(!u.Prioridad.vacia() || !u.Normal.vacia()){
+        c.push(u);
+    }
+}
+
+void Impresora::cancelar_trabajos_usuario(const Usuario& u){
+    ColaDin<Usuario> parser;
+
+        while(!c.vacia() && c.frente() == u)
+            parser.push(c.frente());c.pop();
+    
+        
+        c.pop();//Sacamos el usuario buscado
+        //Metemos el resto de los elementos en la cola original
+        while (!c.vacia()){
+            parser.push(c.frente());
+            c.pop();
+        }
+
+        c = parser;//Metemos la pila lista con los elementos en orden y actualizado
+}    
+
