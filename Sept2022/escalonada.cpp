e791c4068d54a46057030b00d23d978263813117
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-08-12 11:28:41 +0200

    Fin escalonada

--- Sept2022/escalonada.cpp
+++ Sept2022/escalonada.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include "tads/ListaEnla.h"
+#include <climits>
 
 using namespace std;//Avoid using std:: ....
 
@@ -31,6 +32,9 @@ public:
     void imprimir_funcion();
     void eliminar_salto(const int& x);
     int valor_en_x(const int& x);
+    int valor_maximo();
+    int valor_minimo();
+    void traslacion(const int& w, const int& z);
 };
 
 
@@ -40,6 +44,8 @@ int main(){
     E.insertar_salto(-5, 20);
     E.insertar_salto(-6,10);
     E.imprimir_funcion();
+
+    cout<< E.valor_en_x(-8) <<endl;
 
 return 0;
 }
@@ -93,9 +99,115 @@ void Escalonada::imprimir_funcion(){
 
 }   
 
+/**
+ * @brief Elimina el salto que corresponda en la posicion x
+ * @param const int& x
+ * @return void
+ */
 void Escalonada::eliminar_salto(const int& x){
     auto p = Funcion.buscar(salto(x));
     assert( p != Funcion.fin() && "El salto introducido no esta la funcion");
     Funcion.eliminar(p);
 }
 
+/**
+ * @brief Devuelve el valor de la funcion en el punto x de la funcion
+ * @param const int& x
+ * @return int, en caso de no haber valor devuelve INT_MIN
+ */
+int Escalonada::valor_en_x(const int& x){
+    
+    auto i = Funcion.primera();
+    
+    while ( i != Funcion.fin() && Funcion.elemento(i).x < x){
+        i = Funcion.siguiente(i);
+    }
+    
+    //Si nos quedamos en el primero entonces no hay valor a la izquierda
+    if(i == Funcion.primera())
+        return INT_MIN;
+    else if (i == Funcion.fin())
+        return Funcion.elemento(Funcion.anterior(i)).y;
+    else
+        return Funcion.elemento(i).y;
+
+}
+
+/**
+ * @brief Devuelve el maximo valor que toma Y 
+ * @param none
+ * @return int, en caso de vacia INT_MIN
+ */
+int Escalonada::valor_maximo(){
+
+    if(Funcion.primera() == Funcion.fin()){
+        //La lista esta vacia, por tanto devuelvo int min
+        return INT_MIN;
+    }
+    else{
+        
+        auto p = Funcion.primera();
+        int maximo;//-INF
+        maximo = INT_MIN;
+        
+        while ( p != Funcion.fin()){
+            if( maximo < Funcion.elemento(p).y){
+                maximo = Funcion.elemento(p).y;
+            }
+            p = Funcion.siguiente(p);
+        }
+
+        return maximo;
+    }
+
+}
+
+
+/**
+ * @brief Devuelve el maximo valor que toma Y 
+ * @param none
+ * @return int, en caso de vacia INT_MAX
+ */
+int Escalonada::valor_minimo(){
+
+    if(Funcion.primera() == Funcion.fin()){
+        //La lista esta vacia, por tanto devuelvo int min
+        return INT_MAX;
+    }
+    else{
+        
+        auto p = Funcion.primera();
+        int minimo;//-INF
+        minimo = INT_MAX;
+        
+        while ( p != Funcion.fin()){
+            if( minimo > Funcion.elemento(p).y){
+                minimo = Funcion.elemento(p).y;
+            }
+            p = Funcion.siguiente(p);
+        }
+
+        return minimo;
+    }
+
+}
+
+/**
+ * @brief Realiza una traslacion w unidades horizontalmente y z unidades verticalmente
+ * @param const int& w, const int& z
+ * @return none
+ */
+void Escalonada::traslacion(const int& w, const int& z){
+
+    assert(Funcion.fin() == Funcion.primera() && "La funcion no puede estar vacia");
+
+    auto p = Funcion.primera();
+    while ( p != Funcion.fin()){
+        
+        Funcion.elemento(p).x += w;
+        Funcion.elemento(p).y += z;
+
+        p = Funcion.siguiente(p);
+    }
+
+}
