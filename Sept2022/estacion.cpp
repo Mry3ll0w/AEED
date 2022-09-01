b6b0e648ef37e45e3900c91f659dce42a1bf2afa
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-09-01 16:44:56 +0200

    preparacion tad estacion

---
+++ Sept2022/estacion.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;//Avoid using std:: ....
+
+/**
+ * ENUNCIADO:
+ * Una estascion se compone de N vias para el transito de:
+ * - trenes (solo puede haber un tren en cada 1)
+ * - Una via muerta para estacionamiento de trenes ==> trenes sin salida inmmediata
+ * - Via muerta de Maniobras => Solo para el movimiento de trenes dentro de la estación
+ * cuando sea necesario (ambas de capacidad ilimitada).
+ * 
+ * Tren se indentifica con un codigo de tren.
+ * 
+ * Operaciones de tren:
+ * Crear una estacion de n vias de transito.
+ * Llegada de un tren a una via libre dada.
+ * Salida de la estacion del tren situado en una vía dada.
+ * Llevar un tren determinado desde la vía dada a la via de estacionamiento.
+ * Llevar un tren determinado dese la via de estacionamiento a una via de tránsito.
+ * Vía en la que se encuentra un tren.
+ * 
+ * Suponiendo que ya tenemos la implementación de todo el tad anterior (lo de arriba).
+ * Diseña una función que averigue el estado en el que se encuentra la estación a una hora
+ * determinada (que trenes se encuentran en que vías), a partir de la información relativa al
+ * horario de salidas y llegadas del tren-
+ * 
+ * 1) Diseña una estructura para almacenar la información relativa al horario de salidas y 
+ * llegadas de trenes.
+ * 
+ * 2) Dada una estación y el horario de trenes (en orden creciente de horas y minutos), 
+ * implementa una función que devuelva el estado de la estación a una hora determinada.
+ * 
+ * Suponemos que todos los trenes parten de la estación estan en alguna via, ya sea porque han 
+ * efectuado su llegada previamente o bien porque se encontraban en la estación desde el 
+ * principio del dia.
+ * 
+ */
+
+int main(){
+    
+return 0;
+}
