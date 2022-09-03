4c2ac999b49e307a68ff8c028394202aa3d4669c
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-09-03 13:47:36 +0200

    tad estacion

--- Sept2022/estacion.cpp
+++ Sept2022/estacion.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include "ListaEnla.h"
 using namespace std;//Avoid using std:: ....
 
 /**
@@ -35,6 +37,99 @@ using namespace std;//Avoid using std:: ....
  * principio del dia.
  * 
  */
+
+struct Tren{
+    string Hora_llegada, Hora_Salida, id;
+    Tren(string h_l = "", string h_s="", string id_=""):Hora_llegada(h_l), 
+    Hora_Salida(h_s), id(id_){}
+};
+
+struct Via{
+    size_t capacidad;
+    bool ilimitada;
+    ListaEnla<Tren> v;
+    Via(size_t c = 0, bool lim = false):capacidad(c), ilimitada(lim){}
+    bool operator ==(const Via& v){}
+};
+
+class Estacion{
+    ListaEnla<Via> vias_transito;
+    Via Muerta, Muerta_Maniobra;
+    public:
+        Estacion(size_t n_transito=1){}//Crea n vias vacias
+        void inserta_tren(const Via& v, const Tren t){}
+        void sacar_tren_via(const Via &v, const Tren t){}
+        void sacar_tren_estacion(const Tren& t){}
+        const Via muerta(){return Muerta;}
+        const Via muerta_maniobra(){return Muerta_Maniobra;}
+        const ListaEnla<Via> vias_de_transito(){return vias_transito;}
+};
+
+//Funcion auxiliar para ver la hora
+bool hora_menor(string h1, string h2){}
+
+/**
+ * @brief Dada una estación y el horario de trenes (en orden creciente de horas y minutos),
+ * implementa una función que devuelva el estado de la estación a una hora determinada.
+ */
+void estado_estacion(Estacion  E, ListaEnla<string> Horario){
+
+    Via muerta, m_muerta;
+
+    muerta = E.muerta();
+    m_muerta = E.muerta_maniobra();
+
+    ListaEnla<Tren> han_salido, no_han_salido;
+
+    cout<<"En via muerta: "<<endl;
+    for(auto i = muerta.v.primera(); i != muerta.v.fin(); i = muerta.v.siguiente(i)){
+        cout << muerta.v.elemento(i).id <<", " <<endl;
+    }
+
+    cout<<"En vias de maniobra muerta: "<<endl;
+    for (auto i = m_muerta.v.primera(); i != m_muerta.v.fin(); i = m_muerta.v.siguiente(i))
+    {
+        cout << m_muerta.v.elemento(i).id << ", " << endl;
+    }
+
+    //Recorremos el horario (lista de horas)
+    for(auto it_h = Horario.primera(); it_h != Horario.fin(); it_h = Horario.siguiente(it_h)){
+
+        // Vemos como estan las vias de transito
+        for (auto i = E.vias_de_transito().primera(); i != E.vias_de_transito().fin();
+            i = E.vias_de_transito().siguiente(i))
+        {
+            auto via_actual = E.vias_de_transito().elemento(i).v;
+            for (auto it_actual = via_actual.primera(); it_actual != via_actual.fin();
+            it_actual = via_actual.siguiente(it_actual))
+            {
+                //si la hora es menor ==> no han salido los trenes
+                if (hora_menor(Horario.elemento(it_h), via_actual.elemento(it_actual).id))
+                {
+                    no_han_salido.insertar(via_actual.elemento(it_actual), no_han_salido.fin());
+                }
+                else{//han salido los trenes
+                    han_salido.insertar(via_actual.elemento(it_actual), han_salido.fin());
+                }
+                
+            }
+            
+        }
+    }
+
+    //Tras dividir en los trenes que han salido y los que no simplemente los mostramos por 
+    //pantalla
+    cout<< "Los trenes que ya han salido son : "<<endl;
+    for(auto i = han_salido.primera(); i != han_salido.fin(); i = han_salido.siguiente(i)){
+        cout<<"Tren: "<<han_salido.elemento(i).id<<endl;
+    }
+    
+    cout<<"Trenes que quedan por salir: "<<endl;
+    for (auto i = no_han_salido.primera(); i != no_han_salido.fin(); i = no_han_salido.siguiente(i))
+    {
+        cout << "Tren: " << no_han_salido.elemento(i).id << endl;
+    }
+}
 
 int main(){
     
